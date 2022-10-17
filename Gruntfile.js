e6f18026737c34052139f90fad427c41ae1c1b5f
Ezequiel Buffa <soyezequielbuffa@gmail.com>
2022-10-17 16:58:34 -0300

    Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,12 @@
 const { registerTask } = require("grunt");
 
+
 module.exports = function (grunt){
+    require('time-grunt')(grunt);
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin'
+    });
+
     grunt.initConfig({
         sass: {
             dist: {
@@ -17,11 +23,142 @@ module.exports = function (grunt){
         watch: {
             files: ['css/*.scss'],
             tasks:['css']
-        }
+        },
+        browserSync: {
+            bsFiles: {
+                src : [
+                    'css/*.css', 
+                    '*.html',
+                    'js/*.js'
+                ]
+            },
+            options: {
+                watchTask: true,
+                server: {
+                    baseDir: "./"
+                }
+            }
+        },
+        
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: './',
+                    src: ['img/*.{png,jpg,gif,jpeg}'],
+                    dest: 'dist/'
+                }]
+            }
+        },
+
+        copy: {
+            html: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: 'dist'
+                }]
+            },
+            fonts: {
+                files: [{
+                    expand:true,
+                    dot: true,
+                    cwd:'node_modules/open-iconic/font',
+                    src: ['fonts/*.*'],
+                    dest: 'dist'
+                }]
+            }
+          },
+
+          clean: {
+            build: {
+                src: ['dist/']
+            }
+          },
+
+          cssmin: {
+            dist: {}
+          },
+
+          uglify: {
+            dist: {}
+          },
+
+          filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                lenght: 20
+            },
+
+            release: {
+                files: [{
+                    src: [
+                        'dist/js/*.js',
+                        'dist/css/*.css',
+                    ]
+                }]
+            }
+          },
+
+          concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {}
+          },
+
+          useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['index.html', 'about.html', 'contact.html', 'price.html', 'terms.html']
+            },
+            options: {
+                flow:{
+                    steps: {
+                        css:['cssmin'],
+                        js: ['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function(context, block) {
+                                var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0,
+                                    rebase: false
+                                }
+                            }
+                        }]
+                    }
+                }
+            }
+          },
+
+          usemin: {
+            html: ['dist/index.html', 'dist/about.html', 'dist/contact.html', 'dist/price.html', 'dist/terms.html' ],
+            options: {
+                assetsDir: ['dist', 'dist/css', 'dist/js']
+            }
+          }
+
     });
 
 
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt-registerTask('css', ['sass']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('img:compress', ['imagemin']);
+    grunt.registerTask('build', [
+        'clean',
+        'copy',
+        'imagemin',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin'
+    ])
 };
